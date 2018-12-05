6cdcab742ef6d349e2a5525a828af116cd8b9582
sofiasoldano <30936938+sofiasoldano@users.noreply.github.com>
2018-12-05 15:26:01 -0300

    Core Journals (Inicio) - Desktop y mobile

---
+++ core-journals-inicio.php
@@ -0,0 +1,225 @@
+<?php $page_name = "core-journals" ?>
+<?php include('header.php'); ?>
+
+<section class="submenu mb">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="core-menu">
+                    <ul>
+                        <li class="active">
+                            <a href="">
+                                <img src="img/icons/core-1.png" class="img-responsive">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src="img/icons/core-2.png" class="img-responsive">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src="img/icons/core-3.png" class="img-responsive">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src="img/icons/core-4.png" class="img-responsive">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src="img/icons/core-5.png" class="img-responsive">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src="img/icons/core-6.png" class="img-responsive">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src="img/icons/core-7.png" class="img-responsive">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src="img/icons/core-8.png" class="img-responsive">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="main-title text-center">Core Journals <a href="#"><img src="img/icons/alert.png" width="20"></a></div>
+            </div>
+            <div class="col-md-12">
+                <div class="core-tabs left">
+                    <ul>
+                        <li class="active orange">
+                            <a href="#">Inicio</a>
+                        </li>
+                        <li>
+                            <a href="#">Ranking</a>
+                        </li>
+                        <li>
+                            <a href="#">Mi Core</a>
+                        </li>
+                        <li>
+                            <a href="#">Acceso libre</a>
+                        </li>
+                        <li>
+                            <a href="#">Todas</a>
+                        </li>
+                        <li>
+                            <a href="#">Pedido por cita bibliografica</a>
+                        </li>
+                    </ul>   
+                </div>
+                <div class="dropdown right">
+                    <select name="rubros">
+                        <option value="cardiologia">Cardiologia</option>
+                        <option value="cardiologia">Cardiologia</option>
+                        <option value="cardiologia">Cardiologia</option>
+                        <option value="cardiologia">Cardiologia</option>
+                        <option value="cardiologia">Cardiologia</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="inicio">
+    <div class="container">
+        <div class="row">
+            <div class="col-4 col-md-2">
+                <div class="magazine">
+                    <a href="#">
+                        <div class="feature-image">
+                            <img src="img/magazine-1.png" class="img-responsive">
+                        </div>
+                        <p>Archives of disease in childhood</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col-4 col-md-2">
+                <div class="magazine">
+                    <a href="#">
+                        <div class="feature-image">
+                            <img src="img/magazine-1.png" class="img-responsive">
+                        </div>
+                        <p>Archives of disease in childhood</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col-4 col-md-2">
+                <div class="magazine">
+                    <a href="#">
+                        <div class="feature-image">
+                            <img src="img/magazine-1.png" class="img-responsive">
+                        </div>
+                        <p>Archives of disease in childhood</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col-4 col-md-2">
+                <div class="magazine">
+                    <a href="#">
+                        <div class="feature-image">
+                            <img src="img/magazine-1.png" class="img-responsive">
+                        </div>
+                        <p>Archives of disease in childhood</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col-4 col-md-2">
+                <div class="magazine">
+                    <a href="#">
+                        <div class="feature-image">
+                            <img src="img/magazine-1.png" class="img-responsive">
+                        </div>
+                        <p>Archives of disease in childhood</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col-4 col-md-2">
+                <div class="magazine">
+                    <a href="#">
+                        <div class="feature-image">
+                            <img src="img/magazine-1.png" class="img-responsive">
+                        </div>
+                        <p>Archives of disease in childhood</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+         <div class="row">
+            <div class="col-4 col-md-2">
+                <div class="magazine">
+                    <a href="#">
+                        <div class="feature-image">
+                            <img src="img/magazine-1.png" class="img-responsive">
+                        </div>
+                        <p>Archives of disease in childhood</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col-4 col-md-2">
+                <div class="magazine">
+                    <a href="#">
+                        <div class="feature-image">
+                            <img src="img/magazine-1.png" class="img-responsive">
+                        </div>
+                        <p>Archives of disease in childhood</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col-4 col-md-2">
+                <div class="magazine">
+                    <a href="#">
+                        <div class="feature-image">
+                            <img src="img/magazine-1.png" class="img-responsive">
+                        </div>
+                        <p>Archives of disease in childhood</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col-4 col-md-2">
+                <div class="magazine">
+                    <a href="#">
+                        <div class="feature-image">
+                            <img src="img/magazine-1.png" class="img-responsive">
+                        </div>
+                        <p>Archives of disease in childhood</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col-4 col-md-2">
+                <div class="magazine">
+                    <a href="#">
+                        <div class="feature-image">
+                            <img src="img/magazine-1.png" class="img-responsive">
+                        </div>
+                        <p>Archives of disease in childhood</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col-4 col-md-2">
+                <div class="magazine">
+                    <a href="#">
+                        <div class="feature-image">
+                            <img src="img/magazine-1.png" class="img-responsive">
+                        </div>
+                        <p>Archives of disease in childhood</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <a href="#" class="border-btn orange"><p>Ver todas las revistas de cardiología</p></a>
+        </div>
+    </div>
+</section>
+
+<?php include('footer.php'); ?>

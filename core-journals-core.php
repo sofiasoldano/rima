3e09511109a864b430277d68a7b148793ea09b2a
sofiasoldano <30936938+sofiasoldano@users.noreply.github.com>
2018-12-07 15:09:03 -0300

    Core Journals (Mi Core) - Desktop y mobile

---
+++ core-journals-core.php
@@ -0,0 +1,49 @@
+<?php $core_int = "core" ?>
+<?php include('core-journals-submenu.php'); ?>
+
+<div class="mi-core">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="title">Mi core</div>
+                
+                <div class="folders-container">
+                    <a href="#" class="add-folder">
+                        <img src="img/icons/add-folder.png" width="40">
+                        <p>Nueva carpeta</p>
+                    </a>
+                    <a href="#" class="folder pink">
+                        <img src="img/icons/folder.png" width="47">
+                        <p>Para compartir <span class="quantity">8 artículos</span></p>
+                    </a>
+                    <a href="#" class="folder green">
+                        <img src="img/icons/folder.png" width="47">
+                        <p>Sin título <span class="quantity">18 artículos</span></p>
+                    </a>
+                    <a href="#" class="folder light-blue">
+                        <img src="img/icons/folder.png" width="47">
+                        <p>Pediatría <span class="quantity">18 artículos</span></p>
+                    </a>
+                    <a href="#" class="folder purple">
+                        <img src="img/icons/folder.png" width="47">
+                        <p>Temas varios <span class="quantity">18 artículos</span></p>
+                    </a>
+                    <a href="#" class="folder red">
+                        <img src="img/icons/folder.png" width="47">
+                        <p>Sin título <span class="quantity">18 artículos</span></p>
+                    </a>
+                    <a href="#" class="folder light-gray">
+                        <img src="img/icons/folder.png" width="47">
+                        <p>Sin título <span class="quantity">18 artículos</span></p>
+                    </a>
+                    <a href="#" class="folder orange">
+                        <img src="img/icons/folder.png" width="47">
+                        <p>Favoritas <span class="quantity">18 artículos</span></p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('footer.php'); ?>

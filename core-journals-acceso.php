91135e413e56bc24f4483d32246495181d58644e
sofiasoldano <30936938+sofiasoldano@users.noreply.github.com>
2018-12-07 12:09:44 -0300

    Core Journals (Acceso libre) - Desktop y mobile

---
+++ core-journals-acceso.php
@@ -0,0 +1,122 @@
+<?php $core_int = "acceso" ?>
+<?php include('core-journals-submenu.php'); ?>
+
+<div class="acceso-libre">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 hidden-md">
+                <div class="title left">Revista de acceso libre</div>
+                <div class="search right">
+                    <form>
+                        <input type="search" placeholder="Ingrese el nombre o parte del nombre de la revista:" class="input-search">
+                        <button type="submit" value="Enviar" class="button-search"><img src="img/icons/search.png"  width="18"></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="row magazine-list">
+                <div class="col-11">
+                    <div class="list-block">
+                        <a href="#">Acta Dermato - Venereologica  Vol. 98 (10) 5/11/2018</a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Acta Diabetologica  Vol. 55 (9) 1/9/2018</a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Acta Gastroenterológica Latinoamericana  Vol. 48 (3) 1/9/2018</a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Acta Haematologica  Vol. 140 (3) 1/11/2018</a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Acta Histochemica et Cytochemica  Vol. 51 (3) 26/6/2018</a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Acta Informatica Medica  Vol. 26 (2) 1/6/2018</a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Acta Medica Okayama  Vol. 72 (5) 1/10/2018</a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Acta Médica Portuguesa  Vol. 31 (10) 31/10/2018</a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Acta Neurobiologiae Experimentalis  Vol. 78 (3) 1/9/2018</a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Abdominal Imaging  <span class="vol"> Vol. 40 (7) 1/10/2015</span></a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Abdominal Radiology <span class="vol">  Vol. 43 (12) 1/12/2018</a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Academic Emergency Medicine  <span class="vol"> Vol. 25 (10) 1/10/2018</span></a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Academic Medicine <span class="vol"> Vol. 93 (11) 1/11/2018</span></a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Academic Pediatrics <span class="vol"> Vol. 18 (8) 1/11/2018</span></a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Academic Radiology <span class="vol"> Vol. 25 (11) 1/11/2018</span></a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Accounts of Chemical Research <span class="vol"> Vol. 51 (10) 16/10/2018</span></a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Acta Anaesthesiologica Scandinavica <span class="vol"> Vol. 62 (10) 1/11/2018</span></a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Acta Biochimica Polonica <span class="vol"> Vol. 65 (2) 1/6/2018</span></a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Acta Cardiologica <span class="vol"> Vol. 72 (4) 1/8/2017</span></a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Acta Cardiologica Sinica <span class="vol"> Vol. 34 (5) 1/9/2018</span></a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Acta Cirurgica Brasileira <span class="vol"> Vol. 33 (9) 1/9/2018</span></a>
+                    </div>
+                    <div class="list-block">
+                        <a href="#">Acta Cytologica <span class="vol"> Vol. 62 (4) 1/7/2018</span></a>
+                    </div>
+                    
+                </div>
+                <div class="col-1">
+                    <ul class="letters-menu">
+                        <li class="active"><a href="#">a</a></li>
+                        <li><a href="#">b</a></li>
+                        <li><a href="#">c</a></li>
+                        <li><a href="#">d</a></li>
+                        <li><a href="#">e</a></li>
+                        <li><a href="#">f</a></li>
+                        <li><a href="#">g</a></li>
+                        <li><a href="#">h</a></li>
+                        <li><a href="#">i</a></li>
+                        <li><a href="#">j</a></li>
+                        <li><a href="#">k</a></li>
+                        <li><a href="#">l</a></li>
+                        <li><a href="#">m</a></li>
+                        <li><a href="#">n</a></li>
+                        <li><a href="#">o</a></li>
+                        <li><a href="#">p</a></li>
+                        <li><a href="#">q</a></li>
+                        <li><a href="#">r</a></li>
+                        <li><a href="#">s</a></li>
+                        <li><a href="#">t</a></li>
+                        <li><a href="#">u</a></li>
+                        <li><a href="#">v</a></li>
+                        <li><a href="#">w</a></li>
+                        <li><a href="#">x</a></li>
+                        <li><a href="#">y</a></li>
+                        <li><a href="#">z</a></li>
+                        <li><a href="#">#</a></li>
+                    </ul>
+                </div>
+        </div>
+    </div>
+</div>
+
+<?php include('footer.php'); ?>

f6953345a1305e996bd40fec2027edd1efed25a9
sofiasoldano <30936938+sofiasoldano@users.noreply.github.com>
2018-12-11 13:53:17 -0300

    Header background -webkit-clip-path

---
+++ core-journals-core-single.php
@@ -0,0 +1,24 @@
+<?php $core_int = "core" ?>
+<?php include('core-journals-submenu.php'); ?>
+
+<div class="mi-core single">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="title">Mi core</div>
+                
+                <div class="folder-header folder pink">
+                    <div class="breadcrumbs left">
+                        <a href="core-journals-core.php" class="before">Mi Core</a><span class="arrow"><img src="img/icons/arrow-next-white.png" width="10"></span><a href="#">Para compartir</a>
+                    </div>
+                    <div class="edit right">
+                        <a href="#"><img src="img/icons/settings-white.png" width="18"><p>Editar carpeta</p></a>
+                    </div>
+                </div>
+                     
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('footer.php'); ?>

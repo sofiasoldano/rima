e4eca9bb0f281482e24373d48095f32493f0f604
sofiasoldano <30936938+sofiasoldano@users.noreply.github.com>
2018-12-18 17:56:09 -0300

    Reporte para investigadores (Single) - Desktop y mobile

---
+++ reporte-single.php
@@ -0,0 +1,97 @@
+<?php include('reporte-submenu.php'); ?>
+
+<div class="folder-single">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="title">Cardiología</div>
+                
+                <div class="folder-header folder light-blue">
+                    <div class="breadcrumbs left">
+                        <a href="core-journals-core.php" class="before">Estrategias</a><span class="arrow"><img src="img/icons/arrow-next-white.png" width="10"></span><a href="#">Cargiología</a>
+                    </div>
+                    <div class="edit right">
+                        <a href="#"><img src="img/icons/settings-white.png" width="18"><p>Editar carpeta</p></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="single">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="floating-menu">
+                    <ul>
+                        <div class="left">
+                            <li><a href="#"><img src="img/icons/language.png" width="20"></a></li>
+                            <li><a href="#"><img src="img/icons/headphones.png" width="21"></a></li>
+                            <li><a href="#"><img src="img/icons/font-size.png" width="27"></a></li>
+                        </div>
+                        <div class="right">
+                            <li><a href="#"><img src="img/icons/share.png" width="16"></a></li>
+                            <li><a href="#"><img src="img/icons/comment.png" width="20"></a></li>
+                            <li><a href="#"><img src="img/icons/bookmark.png" width="14"></a></li>
+                        </div>
+                    </ul>
+                </div>
+                <div class="content">
+                    <div class="title max">Disturbing the Doxa of Patient Safety Comment on "False Dawns and New Horizons in Patient Safety Research and Practice"</div>
+                   <div class="caption borders">
+                        <p>Travaglia, J.</p>
+                        <p>International Journal of Health Policy and Management	2018;Vol 7(9):867 869</p>
+                    </div>
+                   <p>In a recent edition of this journal, Mannion and Braithwaite provide a succinct analysis of the emergence, and ultimately limited impact, of what they term the current ‘Safety I’ movement in healthcare. They describe the arc of this field from denial, through engagement via mechanisms and approaches imported from other industries, to the current situation where, despite ‘best efforts,’ error rates remain stubbornly recalcitrant. In examining the failure of system-wide efforts to produce sustained reductions in errors and adverse events, that article exposes the doxa, or what Bourdieu calls ‘the taken for granted’ which is central to this latest wave of patient safety movement. In this commentary, I would like to take focus on two key elements of Mannion and Braithwaite’s argument: that harm is caused by misguided but otherwise well-intentioned actions and the ‘embracing’ of patient safety. </p>
+                    <p>I then conclude by briefly considering the implications of these for Safety II, particularly as envisaged by the authors as an evolutionary, and therefore linear progression, from Safety I.</p>
+                   
+                    <a href="#" class="border-btn orange"><p>Ver texto completo</p></a>
+                </div>
+            </div>
+        </div>
+        <div class="comments">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="title">Foro de discusión</div>
+                    <div class="discussion blue-bg"></div>
+                </div>
+                <div class="col-md-5">
+                    <div class="title">Artículos relacionados</div>
+                    <div class="main-block">
+                        <div class="info">
+                            <div class="author">Irimu, G.; Ogero, M.; Mbevi, G.; Agweyu, A.; Akech, S.; Julius, T.; Nyamai, R.; Githang'a, D.; Ayieko, P.; English, M.; Clinical Information Network Authors, G.r.oup.</div>
+                            <div class="name">Preserving oral history: 50 years of paediatric nephrology in Europe.</div>
+                            <div class="pages">Páginas 1008 - 1009</div>
+                        </div>
+                        <div class="bottom">
+                            <ul class="btn-list">
+                                <li><a href="#"><img src="img/icons/bookmark.png" width="14"></a></li>
+                                <li><a href="#"><img src="img/icons/view.png" width="18"></a></li>
+                                <li><a href="#"><img src="img/icons/text-align.png" width="14"></a></li>
+                                <li><a href="#"><img src="img/icons/comment.png" width="20"></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="main-block">
+                        <div class="info">
+                            <div class="author">Lagercrantz, H.</div>
+                            <div class="name">Sweden wins over England in child health championship.</div>
+                            <div class="pages">Páginas 1005 - 1006</div>
+                        </div>
+                        <div class="bottom">
+                            <ul class="btn-list">
+                                <li><a href="#"><img src="img/icons/bookmark.png" width="14"></a></li>
+                                <li><a href="#"><img src="img/icons/view.png" width="18"></a></li>
+                                <li><a href="#"><img src="img/icons/text-align.png" width="14"></a></li>
+                                <li><a href="#"><img src="img/icons/comment.png" width="20"></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include('footer.php'); ?>

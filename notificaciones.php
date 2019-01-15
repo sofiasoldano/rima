402bca407d5999b5067a22e91fe43772a74219e9
sofiasoldano <30936938+sofiasoldano@users.noreply.github.com>
2019-01-15 13:34:38 -0300

    Adjustments

--- notificaciones.php
+++ notificaciones.php
@@ -7,36 +7,36 @@
             <div class="col-md-12">
                 <div class="title">Notificaciones</div>
                 
-                <div class="notification active">
+                <a href="#" class="notification active">
                     <img class="icon" src="img/rima-icon.png">
                     <div class="info">
                         <p>Completa tu perfil para que te podamos brindar una mejor experiencia</p>
                     </div>
-                </div>
+                </a>
                 
-                <div class="notification">
+                <a href="#" class="notification">
                     <img class="icon" src="img/icons/core-3.png">
                     <div class="info">
                         <p>Webvinar en vivo</p>
                         <p class="important">Actualización en Otorrinolaringología Pediátrica</p>
                         <p class="date">7/10/18</p>
                     </div>
-                </div>
+                </a>
                 
-                 <div class="notification">
+                 <a href="#" class="notification">
                     <img class="icon" src="img/icons/core-4.png">
                      <div class="info">
                         <p>Hemos encontrado nuevos artículos para tu estrategia de cardiología</p>
                      </div>
-                </div>
+                </a>
                 
-                <div class="notification">
+                <a href="#" class="notification">
                     <img class="icon" src="img/icons/core-8.png">
                     <div class="info">
                         <p>Ya tenés un nuevo cuento para leer en Sotobosque</p>
                         <p class="date">7/10/18</p>
                     </div>
-                </div>
+                </a>
             </div>
         </div>
     </div>

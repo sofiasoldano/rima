67c7c2dd85f66d3fff699550af7b89d35c85896d
sofiasoldano <30936938+sofiasoldano@users.noreply.github.com>
2019-01-15 13:29:13 -0300

    Notificaciones - Mobile

---
+++ notificaciones.php
@@ -0,0 +1,48 @@
+<?php $page_name = "notificaciones" ?>
+<?php include('header.php'); ?>
+
+<div class="notificaciones">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="title">Notificaciones</div>
+                
+                <div class="notification active">
+                    <img class="icon" src="img/rima-icon.png">
+                    <div class="info">
+                        <p>Completa tu perfil para que te podamos brindar una mejor experiencia</p>
+                    </div>
+                </div>
+                
+                <div class="notification">
+                    <img class="icon" src="img/icons/core-3.png">
+                    <div class="info">
+                        <p>Webvinar en vivo</p>
+                        <p class="important">Actualización en Otorrinolaringología Pediátrica</p>
+                        <p class="date">7/10/18</p>
+                    </div>
+                </div>
+                
+                 <div class="notification">
+                    <img class="icon" src="img/icons/core-4.png">
+                     <div class="info">
+                        <p>Hemos encontrado nuevos artículos para tu estrategia de cardiología</p>
+                     </div>
+                </div>
+                
+                <div class="notification">
+                    <img class="icon" src="img/icons/core-8.png">
+                    <div class="info">
+                        <p>Ya tenés un nuevo cuento para leer en Sotobosque</p>
+                        <p class="date">7/10/18</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<?php include('footer.php'); ?>
